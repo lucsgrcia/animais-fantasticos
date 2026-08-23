8bfc54444a32b4334fd4f2a36e5c438a30cb4372
Lucas Garcia <lucas.garciadelacerda@gmail.com>
2026-08-23 16:17:22 -0300

    add slide galery

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
+import Slide from "./modules/slide.js";
 
 const scrollSuave = new SmoothScroll("[data-menu='lista'] a[href^='#']");
 scrollSuave.init();
@@ -43,6 +44,9 @@ menuMobile.init();
 
 const funcionamento = new Funcionamento("[data-semana]", "aberto");
 funcionamento.init();
+
+const slide = new Slide(".slide", ".slide-wrapper");
+slide.init();
 
 fetchAnimais("./animaisapi.json", ".numeros-grid");
 
